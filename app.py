15a2ad5b3f0f0a2ad6055b90f4e4e9b6ee8ec602
KaranKBajpai <mr.karanb@gmail.com>
2025-06-19 14:22:30 -0700

    Add income / expense form

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Budget World!"
+# In-memory data storage (Will upgrade to a real database later)
+entries = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global entries
+    if request.method == 'POST':
+        description = request.form["description"]
+        amount = float(request.form["amount"])
+        type = request.form["type"]
+        entries.append({
+            'description': description,
+            'amount': amount,
+            'type': type
+        })
+        return redirect('/')
+    return render_template('index.html', entries=entries)
 
 if __name__ == '__main__':
     app.run(debug=True)
