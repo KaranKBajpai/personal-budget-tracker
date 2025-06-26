793f91e3f3b5bc1e15194d752a3bce3bf33827f1
KaranKBajpai <mr.karanb@gmail.com>
2025-06-25 18:52:31 -0700

    Added db

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# In-memory data storage (Will upgrade to a real database later)
-entries = []
+db = SQLAlchemy(app)
+
+# # In-memory data storage (Will upgrade to a real database later)
+# entries = []
+
+# Define the database model
+class Entry(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(10), nullable=False)
+
+# Create the database file (only runs once)
+if not os.path.exists('budget.db'):
+    with app.app_context():
+        db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global entries
-
-
     if request.method == 'POST':
         description = request.form["description"]
         amount = float(request.form["amount"])
         type = request.form["type"]
 
-        entries.append({
-            'description': description,
-            'amount': amount,
-            'type': type
-        })
+        new_entry = Entry(description=description, amount=amount, type=type)
+        db.session.add(new_entry)
+        db.session.commit()
 
         return redirect('/')
     
+    entries = Entry.query.all()
+
     # Calculate current balance 
-    income = sum(entry['amount'] for entry in entries if entry['type'] == 'income')
-    expense = sum(entry['amount'] for entry in entries if entry['type'] == 'expense')
+    income = sum(entry.amount for entry in entries if entry.type == 'income')
+    expense = sum(entry.amount for entry in entries if entry.type == 'expense')
     balance = income - expense
 
     return render_template('index.html', entries=entries, balance=balance)
